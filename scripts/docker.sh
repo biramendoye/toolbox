da640a18ccd222d5a72ef935ebda1682420c2df1
Bakarseck <86267786+Bakarseck@users.noreply.github.com>
2025-03-27 21:43:33 +0000

    Add `-y` when installing

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,7 +7,7 @@ echo "+----------------+"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
