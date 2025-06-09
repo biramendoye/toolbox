ebb258673bcdf77e0c87082259d539b02c3184d6
bakseck <seck.bakar@ugb.edu.sn>
2025-06-09 17:41:59 +0000

    add ci/cd

---
+++ scripts/install-docker.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo "+----------------+"
+echo "+ INSTALL DOCKER +"
+echo "+----------------+"
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+chmod g+rwx "$HOME/.docker" -R
